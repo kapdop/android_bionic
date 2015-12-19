spsforks/android-bionic-libc

refactoring: introduce MappedFileFragment
Change-Id: I97b0c1131711ed0ac13ab33e3357e99e0b6b1a4e

linker/Android.mk
@@ -12,6 +12,7 @@ LOCAL_SRC_FILES := \
     linker_sdk_versions.cpp \
     linker_block_allocator.cpp \
     linker_libc_support.c \
+    linker_map.cpp \
     linker_memory.cpp \
     linker_phdr.cpp \
     linker_utils.cpp \

kapdop/android_bionic

linker/Android.mk
@@ -12,6 +12,7 @@ LOCAL_SRC_FILES := \
     linker_sdk_versions.cpp \
     linker_block_allocator.cpp \
     linker_libc_support.c \
+    linker_mapped_file_fragment.cpp \
     linker_memory.cpp \
     linker_phdr.cpp \
     rt.cpp \